63c09534ebf4f7c21f67b94897065320acf05d4a
Christian <christian.maigua@hotmail.com>
2021-11-02 23:14:46 -0300

    Subiendo clase 3

---
+++ js/main.js
@@ -0,0 +1,5 @@
+let ingresarNumero = parseInt(prompt("Ingresar número"));
+for (let i = 1; i <= 10; i++) {
+  let resultado = ingresarNumero + i;
+  alert(ingresarNumero + " + " + i + " = " + resultado);
+}
